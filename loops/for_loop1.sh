78fcd1009ca176ddde0fc826312295908d7d2df0
anchal <credential.helper>
2024-02-08 15:13:05 -0500

    removed file

--- loops/for_loop1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in range 3 4 5 6 7
-do 
-	echo " Number is $i "
-done
--- loops/for_with_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Getting values from the file
-FILE="/home/kali/script/centralgit/loops/members.txt"
-for name in $(cat $FILE)
-do
-	echo "Name is $name"
-done
-
